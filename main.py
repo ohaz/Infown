59f3f892ae9022375260309678b367d4cfeca3bd
Richard Baumann <ohaz@endless-aerospace.de>
2017-08-18 16:03:12 +0200

    Add tornado server

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template, request
 import os
 import json
 from app import app
+from config import port
+
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 import views
 
 if __name__ == '__main__':
-    app.run()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(port)
+    IOLoop.instance().start()
